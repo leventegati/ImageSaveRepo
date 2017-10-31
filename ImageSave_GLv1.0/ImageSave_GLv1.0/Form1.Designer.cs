4c875c144c6dae5dc71a11dbb8c17a7319ad14ea
leventegati <gatilevente@gmail.com>
2017-10-31 11:15:50 +0100

    Repo filled with the current files
    Initiation

---
+++ ImageSave_GLv1.0/ImageSave_GLv1.0/Form1.Designer.cs
@@ -0,0 +1,350 @@
+﻿namespace ImageSave_GLv1._0
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.labelInfo = new System.Windows.Forms.Label();
+            this.buttonBrowse = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelPath = new System.Windows.Forms.Label();
+            this.textBoxWidth = new System.Windows.Forms.TextBox();
+            this.textBoxHeight = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxExpTime = new System.Windows.Forms.TextBox();
+            this.radioButtonExpModeTimed = new System.Windows.Forms.RadioButton();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxOffsetY = new System.Windows.Forms.TextBox();
+            this.textBoxOffsetX = new System.Windows.Forms.TextBox();
+            this.labelAddInfo = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.buttonTrigger = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Enabled = false;
+            this.buttonStart.Location = new System.Drawing.Point(19, 88);
+            this.buttonStart.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(79, 31);
+            this.buttonStart.TabIndex = 4;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(185, 28);
+            this.textBox.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.ReadOnly = true;
+            this.textBox.Size = new System.Drawing.Size(164, 91);
+            this.textBox.TabIndex = 3;
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Enabled = false;
+            this.buttonStop.Location = new System.Drawing.Point(102, 88);
+            this.buttonStop.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(79, 31);
+            this.buttonStop.TabIndex = 5;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Location = new System.Drawing.Point(19, 28);
+            this.buttonSearch.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(79, 31);
+            this.buttonSearch.TabIndex = 1;
+            this.buttonSearch.Text = "Search";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
+            // 
+            // labelInfo
+            // 
+            this.labelInfo.AutoSize = true;
+            this.labelInfo.Location = new System.Drawing.Point(23, 195);
+            this.labelInfo.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelInfo.Name = "labelInfo";
+            this.labelInfo.Size = new System.Drawing.Size(59, 13);
+            this.labelInfo.TabIndex = 5;
+            this.labelInfo.Text = "Information";
+            this.labelInfo.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // buttonBrowse
+            // 
+            this.buttonBrowse.Enabled = false;
+            this.buttonBrowse.Location = new System.Drawing.Point(100, 28);
+            this.buttonBrowse.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonBrowse.Name = "buttonBrowse";
+            this.buttonBrowse.Size = new System.Drawing.Size(79, 31);
+            this.buttonBrowse.TabIndex = 2;
+            this.buttonBrowse.Text = "Browse";
+            this.buttonBrowse.UseVisualStyleBackColor = true;
+            this.buttonBrowse.Click += new System.EventHandler(this.buttonBrowse_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(184, 10);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Connected device";
+            // 
+            // labelPath
+            // 
+            this.labelPath.AutoSize = true;
+            this.labelPath.Location = new System.Drawing.Point(23, 219);
+            this.labelPath.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelPath.Name = "labelPath";
+            this.labelPath.Size = new System.Drawing.Size(29, 13);
+            this.labelPath.TabIndex = 8;
+            this.labelPath.Text = "Path";
+            this.labelPath.Visible = false;
+            // 
+            // textBoxWidth
+            // 
+            this.textBoxWidth.Location = new System.Drawing.Point(4, 44);
+            this.textBoxWidth.Margin = new System.Windows.Forms.Padding(2);
+            this.textBoxWidth.Name = "textBoxWidth";
+            this.textBoxWidth.Size = new System.Drawing.Size(76, 20);
+            this.textBoxWidth.TabIndex = 9;
+            // 
+            // textBoxHeight
+            // 
+            this.textBoxHeight.Location = new System.Drawing.Point(4, 81);
+            this.textBoxHeight.Margin = new System.Windows.Forms.Padding(2);
+            this.textBoxHeight.Name = "textBoxHeight";
+            this.textBoxHeight.Size = new System.Drawing.Size(76, 20);
+            this.textBoxHeight.TabIndex = 10;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(4, 28);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Width";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(4, 65);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Height";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.textBoxExpTime);
+            this.groupBox1.Controls.Add(this.radioButtonExpModeTimed);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.textBoxOffsetY);
+            this.groupBox1.Controls.Add(this.textBoxOffsetX);
+            this.groupBox1.Controls.Add(this.textBoxWidth);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBoxHeight);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(364, 10);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Size = new System.Drawing.Size(174, 198);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Camera Settings";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(4, 136);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(77, 13);
+            this.label6.TabIndex = 20;
+            this.label6.Text = "Exposure Time";
+            // 
+            // textBoxExpTime
+            // 
+            this.textBoxExpTime.Location = new System.Drawing.Point(4, 152);
+            this.textBoxExpTime.Name = "textBoxExpTime";
+            this.textBoxExpTime.Size = new System.Drawing.Size(76, 20);
+            this.textBoxExpTime.TabIndex = 19;
+            // 
+            // radioButtonExpModeTimed
+            // 
+            this.radioButtonExpModeTimed.AutoSize = true;
+            this.radioButtonExpModeTimed.Location = new System.Drawing.Point(10, 107);
+            this.radioButtonExpModeTimed.Name = "radioButtonExpModeTimed";
+            this.radioButtonExpModeTimed.Size = new System.Drawing.Size(100, 17);
+            this.radioButtonExpModeTimed.TabIndex = 18;
+            this.radioButtonExpModeTimed.TabStop = true;
+            this.radioButtonExpModeTimed.Text = "Timed exposure";
+            this.radioButtonExpModeTimed.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(82, 65);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(45, 13);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Offset Y";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(82, 28);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Offset X";
+            // 
+            // textBoxOffsetY
+            // 
+            this.textBoxOffsetY.Location = new System.Drawing.Point(84, 81);
+            this.textBoxOffsetY.Margin = new System.Windows.Forms.Padding(2);
+            this.textBoxOffsetY.Name = "textBoxOffsetY";
+            this.textBoxOffsetY.Size = new System.Drawing.Size(76, 20);
+            this.textBoxOffsetY.TabIndex = 15;
+            // 
+            // textBoxOffsetX
+            // 
+            this.textBoxOffsetX.Location = new System.Drawing.Point(85, 43);
+            this.textBoxOffsetX.Margin = new System.Windows.Forms.Padding(2);
+            this.textBoxOffsetX.Name = "textBoxOffsetX";
+            this.textBoxOffsetX.Size = new System.Drawing.Size(76, 20);
+            this.textBoxOffsetX.TabIndex = 14;
+            // 
+            // labelAddInfo
+            // 
+            this.labelAddInfo.AutoSize = true;
+            this.labelAddInfo.Location = new System.Drawing.Point(23, 240);
+            this.labelAddInfo.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelAddInfo.Name = "labelAddInfo";
+            this.labelAddInfo.Size = new System.Drawing.Size(107, 13);
+            this.labelAddInfo.TabIndex = 14;
+            this.labelAddInfo.Text = "Additional information";
+            this.labelAddInfo.Visible = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // buttonTrigger
+            // 
+            this.buttonTrigger.Enabled = false;
+            this.buttonTrigger.Location = new System.Drawing.Point(19, 144);
+            this.buttonTrigger.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonTrigger.Name = "buttonTrigger";
+            this.buttonTrigger.Size = new System.Drawing.Size(79, 29);
+            this.buttonTrigger.TabIndex = 15;
+            this.buttonTrigger.Text = "Trigger";
+            this.buttonTrigger.UseVisualStyleBackColor = true;
+            this.buttonTrigger.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(562, 300);
+            this.Controls.Add(this.buttonTrigger);
+            this.Controls.Add(this.labelAddInfo);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.labelPath);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonBrowse);
+            this.Controls.Add(this.labelInfo);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.buttonStart);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "mainForm";
+            this.Text = "ImageSave";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.mainForm_FormClosing);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.Label labelInfo;
+        private System.Windows.Forms.Button buttonBrowse;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelPath;
+        private System.Windows.Forms.TextBox textBoxWidth;
+        private System.Windows.Forms.TextBox textBoxHeight;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label labelAddInfo;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button buttonTrigger;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxOffsetY;
+        private System.Windows.Forms.TextBox textBoxOffsetX;
+        private System.Windows.Forms.RadioButton radioButtonExpModeTimed;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxExpTime;
+    }
+}
+
---
+++ ImageSave_GLv1.0/ImageSave_GLv1.0/Form1.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Jai_FactoryDotNET;
+
+//using System.Threading;
+using System.Runtime.InteropServices;
+using System.IO;
+
+namespace ImageSave_GLv1._0
+{
+    public partial class mainForm : Form
+    {
+        CFactory myFactory = new CFactory();
+        CCamera myCamera;
+        string selectedPath;
+        // Bitmap image;
+        String timestmp_folder = DateTime.Now.ToString("yyyy-MM-dd HH_mm_ss");
+        CNode myNode;
+        Jai_FactoryWrapper.EFactoryError error = Jai_FactoryWrapper.EFactoryError.Success;
+        int counter = 0;
+        int timeCounter = 0;
+        string pastepath;
+        string sourceF;
+        string destF;
+    
+
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+        // Exposure Mode
+        // Exposure Time (us)
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+            labelInfo.Text = "Searching for camera...";
+            try
+            {
+                error = myFactory.Open("");
+                cameraSearch();
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show("Camera not found, please make sure the connection is working.");
+                Application.Exit();
+            }
+
+            textBoxWidth.Text = "1000";
+            textBoxHeight.Text = "800";
+            textBoxOffsetX.Text = "600";
+            textBoxOffsetY.Text = "500";
+            buttonBrowse.Enabled = true;
+            buttonSearch.Enabled = false;
+
+        }
+
+        private void buttonBrowse_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                selectedPath = dialog.SelectedPath;
+            }
+            else
+            {
+                selectedPath = "C:\\Users\\gatil\\Desktop\\Képek";
+                pastepath = "C:\\Users\\gatil\\Desktop\\Output\\Képek";
+            }
+          
+            labelInfo.Text = "The pictures will be saved to: ";
+            labelPath.Text = selectedPath;
+            
+
+
+            labelPath.Visible = true;
+            buttonStart.Enabled = true;
+
+
+                textBoxExpTime.Text = Convert.ToString(myCamera.GetNode("ExposureTime").Value);
+            
+
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            cameraStart();
+
+            buttonTrigger.Enabled = true;
+            buttonStart.Enabled = false;
+            buttonStop.Enabled = true;
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            cameraStop();
+
+            buttonTrigger.Enabled = false;
+            buttonStart.Enabled = true;
+            buttonStop.Enabled = false;
+        }
+
+
+        private void cameraStart()
+        {
+            if (myCamera != null)
+            {
+                CNode nodeAcquisitionMode = myCamera.GetNode("AcquisitionMode");
+                if (null != nodeAcquisitionMode)
+                {
+                    nodeAcquisitionMode.Value = "Continuous";
+
+                    myCamera.AcquisitionCount = UInt32.MaxValue;
+                }
+
+
+
+                myCamera.GetNode("Width").Value = Convert.ToInt32(textBoxWidth.Text);
+                myCamera.GetNode("Height").Value = Convert.ToInt32(textBoxHeight.Text);
+                myCamera.GetNode("OffsetX").Value = Convert.ToInt32(textBoxOffsetX.Text);
+                myCamera.GetNode("OffsetY").Value = Convert.ToInt32(textBoxOffsetY.Text);
+
+                labelAddInfo.Visible = true;
+
+                if (radioButtonExpModeTimed.Checked)
+                {
+                    myCamera.GetNode("ExposureMode").Value = "Timed";
+                    myCamera.GetNode("ExposureTime").Value = Convert.ToDouble(textBoxExpTime.Text);
+                }
+
+                myCamera.StartImageAcquisition(true, 5);
+               // myCamera.NewImageDelegate += new Jai_FactoryWrapper.ImageCallBack(imageSaver);
+                timer1.Start();
+
+            }
+        }
+
+        private void cameraStop()
+        {
+            myCamera.StopImageAcquisition();
+            // myCamera.NewImageDelegate -= new Jai_FactoryWrapper.ImageCallBack(imageSaver);
+            timer1.Stop();
+        }
+
+        private void cameraSearch()
+        {
+            if (null != myCamera)
+            {
+                if (myCamera.IsOpen)
+                {
+                    myCamera.Close();
+                }
+
+                myCamera = null;
+            }
+
+            myFactory.UpdateCameraList(Jai_FactoryDotNET.CFactory.EDriverType.FilterDriver);
+            for (int i = 0; i < myFactory.CameraList.Count; i++)
+            {
+                myCamera = myFactory.CameraList[i];
+                if (Jai_FactoryWrapper.EFactoryError.Success == myCamera.Open())
+                {
+                    break;
+                }
+            }
+
+            if (null != myCamera && myCamera.IsOpen)
+            {
+                textBox.Text = myCamera.CameraID;
+                labelInfo.Text = "Camera found!";
+
+               // int currentValue = 0;
+            }
+            else
+            {
+                labelInfo.Text = "No camera found!";
+            }
+
+        }
+
+        unsafe
+        void imageSaver(ref Jai_FactoryWrapper.ImageInfo ImageInfo)
+        {
+            counter += 1;
+            
+            Bitmap image = GetBitmap((int)ImageInfo.SizeX, (int)ImageInfo.SizeY, 8, (byte*)ImageInfo.ImageBuffer);
+            String timestmp = DateTime.Now.ToString("yyyy-MM-dd HH_mm_ss");
+
+            image.Save(selectedPath + "\\" + timestmp + "_L.jpg", ImageFormat.Jpeg);
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (IsDirectoryEmpty(pastepath)) {
+                String timestmp = DateTime.Now.ToString("yyyy-MM-dd HH_mm_ss");
+                myCamera.SaveLastFrame(selectedPath + "\\" + timestmp + "_L.jpg", Jai_FactoryWrapper.ESaveFileFormat.Jpeg, 100);
+                labelAddInfo.Text = "Saving picture: " + timestmp + "_L.jpg";
+                sourceF = selectedPath + "\\" + timestmp + "_L.jpg";
+                destF = pastepath + "\\" + timestmp + "_L.jpg";
+                this.Move(sourceF, destF);
+            }
+        }
+
+        unsafe
+        public Bitmap GetBitmap(int nWidth, int nHeight, int nBpp, byte* DataColor)
+        {
+            Bitmap BitMapImage = new Bitmap(nWidth, nHeight, PixelFormat.Format24bppRgb);
+            BitmapData srcBmpData = BitMapImage.LockBits(new Rectangle(0, 0, BitMapImage.Width, BitMapImage.Height),
+            ImageLockMode.ReadWrite, BitMapImage.PixelFormat);
+
+            switch (BitMapImage.PixelFormat)
+            {
+                case PixelFormat.Format24bppRgb:
+                    unsafe
+                    {
+                        byte* psrcBuffer = (byte*)srcBmpData.Scan0.ToPointer();
+
+                        int nCount = srcBmpData.Width * srcBmpData.Height;
+                        int nIndex = 0;
+
+                        for (int y = 0; y < nCount; y++)
+                        {
+                            psrcBuffer[nIndex++] = DataColor[y];
+                            psrcBuffer[nIndex++] = DataColor[y];
+                            psrcBuffer[nIndex++] = DataColor[y];
+                        }
+                    }
+                    break;
+            }
+
+            BitMapImage.UnlockBits(srcBmpData);
+
+            return BitMapImage;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            String timestmp = DateTime.Now.ToString("yyyy-MM-dd HH_mm_ss");
+            myCamera.SaveLastFrame(selectedPath + "\\" + timestmp + "_L.jpg", Jai_FactoryWrapper.ESaveFileFormat.Jpeg, 100);
+            this.Copy(selectedPath, pastepath);
+            
+        }
+
+        public bool IsDirectoryEmpty(string path)
+        {
+            return !Directory.EnumerateFileSystemEntries(path).Any();
+        }
+
+        void Copy(string sourceDir, string targetDir)
+        {
+            Directory.CreateDirectory(targetDir);
+
+            foreach (var file in Directory.GetFiles(sourceDir))
+                File.Copy(file, Path.Combine(targetDir, Path.GetFileName(file)));
+        }
+
+        void Move(string sourceFile, string destinationFile)
+        {
+            System.IO.File.Move(sourceFile, destinationFile);
+        }
+
+        private void mainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Environment.Exit(1);
+        }
+
+
+    }
+}
